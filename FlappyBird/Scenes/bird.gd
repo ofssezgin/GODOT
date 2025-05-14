6ac15db04d49f943a6892dede599e83e6ffe7f67
Furkan Sezgin <ofssezgin@gmail.com>
2025-05-14 20:58:34 +0300

    bird added

---
+++ FlappyBird/Scenes/bird.gd
@@ -0,0 +1,47 @@
+extends CharacterBody2D
+
+class_name Bird
+
+@export var gravity = 900.0
+@export var jump_force = -300
+@export var rotation_speed = 2
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+var max_speed = 400
+var is_started = false
+
+func _ready():
+	velocity = Vector2.ZERO
+	animation_player.play("idle")
+
+func _physics_process(delta):
+	if Input.is_action_just_pressed("jump"):
+		if !is_started:
+			animation_player.play("flap_wings")
+			is_started = true
+		jump()
+	
+	if !is_started:
+		return
+	velocity.y += gravity * delta
+	
+	velocity.y = min(velocity.y, max_speed)
+	#if velocity .y > max_speed:
+	#	velocity.y = max_speed
+	
+	move_and_collide(velocity * delta)
+	
+	rotate_bird()
+	
+func jump():
+	velocity.y  = jump_force
+	rotation = deg_to_rad(-30)
+
+func rotate_bird():
+	#rotate down
+	if velocity.y > 0 && rad_to_deg(rotation) < 90:
+		rotation += rotation_speed * deg_to_rad(1)
+	#rotate up
+	elif velocity.y < 0 && rad_to_deg(rotation) > -30:
+		rotation -= rotation_speed * deg_to_rad(1)
