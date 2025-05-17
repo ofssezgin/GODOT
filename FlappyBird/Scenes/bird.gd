b1f78cc36dc8d13e5ad5c315df09a9579778b41b
Furkan Sezgin <ofssezgin@gmail.com>
2025-05-17 19:49:33 +0300

    ground added

--- FlappyBird/Scenes/bird.gd
+++ FlappyBird/Scenes/bird.gd
@@ -10,13 +10,14 @@ class_name Bird
 
 var max_speed = 400
 var is_started = false
+var should_process_input = true
 
 func _ready():
 	velocity = Vector2.ZERO
 	animation_player.play("idle")
 
 func _physics_process(delta):
-	if Input.is_action_just_pressed("jump"):
+	if Input.is_action_just_pressed("jump") && should_process_input:
 		if !is_started:
 			animation_player.play("flap_wings")
 			is_started = true
@@ -45,3 +46,11 @@ func rotate_bird():
 	#rotate up
 	elif velocity.y < 0 && rad_to_deg(rotation) > -30:
 		rotation -= rotation_speed * deg_to_rad(1)
+
+func stop():
+	animation_player.stop()
+	gravity = 0
+	velocity = Vector2.ZERO
+	should_process_input = false
+	
+	
